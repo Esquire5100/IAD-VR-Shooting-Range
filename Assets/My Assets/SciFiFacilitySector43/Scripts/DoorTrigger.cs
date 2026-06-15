53147d763298aeee922b095226f2036a6186be74
Esquire5100 <KLEAH.24@ichat.sp.edu.sg>
2026-06-15 18:49:31 +0800

    imported packaged

---
+++ Assets/My Assets/SciFiFacilitySector43/Scripts/DoorTrigger.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorTrigger : MonoBehaviour
+{
+    private FacilDoor door;
+
+    void Start()
+    {
+        door = GetComponentInParent<FacilDoor>();
+    }
+
+    void OnTriggerEnter(Collider c) {
+
+        door.openDoor(c);
+
+    }
+
+    void OnTriggerExit(Collider c) {
+        door.closeDoor(c);
+    }
+}
---
+++ Assets/My Assets/SciFiLevelKit/Scripts/DoorManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorManager : MonoBehaviour {
+
+	public LvlKitDoor door1;
+	public LvlKitDoor door2;
+	
+	
+	void OnTriggerEnter(){
+		
+		if (door1!=null){
+			door1.OpenDoor();	
+
+		}
+		
+		if (door2!=null){
+			door2.OpenDoor();	
+
+		}
+	}
+}
